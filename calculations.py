9cefe71603a414e6e07f73393210ab22605a3c57
Samuel <baceceo@gmail.com>
2025-12-08 23:24:39 +0000

    add price change

--- calculations.py
+++ calculations.py
@@ -1,2 +1,7 @@
+def price_change(p0, p1):
+    return p1 - p0
+
+
 def return_value(start_price, end_price):
-    return (end_price - start_price) / start_price
+    difference = price_change(start_price, end_price)
+    return difference / start_price
