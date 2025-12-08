d5a09376a5d6316cf19ee83cba032ef7464d31e2
Samuel <baceceo@gmail.com>
2025-12-08 23:47:23 +0000

    add growth factor

--- calculations.py
+++ calculations.py
@@ -5,3 +5,7 @@ def price_change(start_price, end_price):
 def return_value(start_price, end_price):
     difference = price_change(start_price, end_price)
     return difference / start_price
+
+
+def growth_factor(start_price, end_price):
+    return 1 + return_value(start_price, end_price)
