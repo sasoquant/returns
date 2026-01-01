9f4745291320cc75275a5f8b8f4335fc2ed73043
Samuel <baceceo@gmail.com>
2026-01-01 11:42:01 +0000

    add comment

--- calculations.py
+++ calculations.py
@@ -7,5 +7,5 @@ def return_value(start_price, end_price):
     return difference / start_price
 
 
-def growth_factor(start_price, end_price):
+def growth_factor(start_price, end_price):  # price relative
     return 1 + return_value(start_price, end_price)
